c0b7e694ea815a261d8113ebde1e03416e409225
Danny Chang <dannychachang@gmail.com>
2017-12-11 13:59:40 -0500

    add age question

--- exercise3.rb
+++ exercise3.rb
@@ -1,5 +1,21 @@
-puts "What is your name?"
+# puts "What is your name?"
 
 user_name = gets
 
 puts "Hello, #{user_name}"
+
+puts "What is your name?"
+
+# gets.chomp
+
+user_name = gets.chomp
+
+puts "Hello, #{user_name}"
+
+# age question
+
+puts "How old are you?"
+
+age = gets.to_i
+
+puts "Ah yes, you must have been born in #{2017 - age}"

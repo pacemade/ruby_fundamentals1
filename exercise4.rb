d24227b3222d7fc0cd14de92add58b58889bcb2d
Danny Chang <dannychachang@gmail.com>
2017-12-11 14:23:06 -0500

    older and less age done

---
+++ exercise4.rb
@@ -0,0 +1,25 @@
+# puts "Please give me a number"
+#
+# number = gets.to_i
+#
+# if number < 100
+#   puts "That's a really big number!"
+# else
+#   puts "Why dream a little bigger?"
+# end
+
+# Age comparison
+
+puts "Please tell me your age"
+
+age = gets.to_i
+
+if age > 105
+  puts "I'm not sure I believe you"
+elsif age > 27
+  puts "Wow, you are #{age - 27} years older than me"
+elsif age < 27
+    puts "Wow, you are #{27 - age} younger than me"
+end
+
+# Same same name

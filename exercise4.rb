6195cbb5f610e9268a0e65e2f71b4804c5bf6e59
Danny Chang <dannychachang@gmail.com>
2017-12-11 14:45:24 -0500

    name check with gets.chomp

--- exercise4.rb
+++ exercise4.rb
@@ -10,16 +10,30 @@
 
 # Age comparison
 
-puts "Please tell me your age"
-
-age = gets.to_i
-
-if age > 105
-  puts "I'm not sure I believe you"
-elsif age > 27
-  puts "Wow, you are #{age - 27} years older than me"
-elsif age < 27
-    puts "Wow, you are #{27 - age} younger than me"
-end
+# puts "Please tell me your age"
+#
+# age = gets.to_i
+#
+# if age > 105
+#   puts "I'm not sure I believe you"
+# elsif age > 27
+#   puts "Wow, you are #{age - 27} years older than me"
+# elsif age < 27
+#     puts "Wow, you are #{27 - age} younger than me"
+# end
 
 # Same same name
+
+realname = "danny"
+
+puts "Please give me your name!"
+
+name = gets.chomp
+
+puts name
+
+if name == realname
+  puts "No way! We have the same name!"
+else
+  puts "This is a decent kind of name"
+end

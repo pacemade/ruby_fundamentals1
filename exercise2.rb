083be6ada86f55dd67d328a2e758508b72d1a19a
Danny Chang <dannychachang@gmail.com>
2017-12-11 13:46:31 -0500

    finish exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+# 20% tip for 55 dollar meal
+
+total = 55
+tip = total * 0.20
+
+puts 55 * 0.20
+
+
+# Adding a string to an integer
+
+puts total + 11
+
+# String interpolation
+
+puts "the result of 45628 multiplied by 7839 is #{45628 * 7839}"
